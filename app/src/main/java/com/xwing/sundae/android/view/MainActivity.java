96ea75fbd8cffcff6fdfbed516d190ebaa7cb771
dukai800 <dukai1410@163.com>
2019-05-13 00:47:31 +0800

    add login activity

--- app/src/main/java/com/xwing/sundae/android/view/MainActivity.java
+++ app/src/main/java/com/xwing/sundae/android/view/MainActivity.java
@@ -1,15 +1,18 @@
 package com.xwing.sundae.android.view;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.ashokvarma.bottomnavigation.ShapeBadgeItem;
 import com.xwing.sundae.R;
 import com.xwing.sundae.android.view.explore.ExploreFragment;
 import com.xwing.sundae.android.view.index.IndexFragment;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements
     private int lastSelectedPosition;
 
     private BottomNavigationBar mBottomNavigationBar;
+    private ShapeBadgeItem mShapeBadgeItem;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
     private IndexFragment mIndexFragment;
@@ -51,13 +55,24 @@ public class MainActivity extends AppCompatActivity implements
         mBottomNavigationBar = findViewById(R.id.navigationBar);
         mBottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
         mBottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
-        mBottomNavigationBar.setBarBackgroundColor(R.color.colorNavigationBarInactivedBg);
+        mBottomNavigationBar.setBarBackgroundColor(R.color.colorNavigationBarBg);
+
+        mShapeBadgeItem = new ShapeBadgeItem()
+                .setShape(ShapeBadgeItem.SHAPE_OVAL)
+                .setShapeColor(R.color.colorPrimary)
+                .setShapeColorResource(R.color.colorPrimary)
+                .setSizeInDp(this,10,10)
+                .setEdgeMarginInDp(this,2)
+//                .setSizeInPixels(30,30)
+//                .setEdgeMarginInPixels(-1)
+                .setGravity(Gravity.TOP | Gravity.END)
+                .setHideOnSelect(true);
 
         mBottomNavigationBar
                 .addItem(new BottomNavigationItem(R.drawable.index_actived, "首页").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.index).setInActiveColorResource(R.color.colorNavigationBarInactivedText))
                 .addItem(new BottomNavigationItem(R.drawable.explore_actived, "发现").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.explore).setInActiveColorResource(R.color.colorNavigationBarInactivedText))
                 .addItem(new BottomNavigationItem(R.drawable.post_actived, "发布").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.post).setInActiveColorResource(R.color.colorNavigationBarInactivedText))
-                .addItem(new BottomNavigationItem(R.drawable.message_actived, "消息").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.message).setInActiveColorResource(R.color.colorNavigationBarInactivedText))
+                .addItem(new BottomNavigationItem(R.drawable.message_actived, "消息").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.message).setInActiveColorResource(R.color.colorNavigationBarInactivedText).setBadgeItem(mShapeBadgeItem))
                 .addItem(new BottomNavigationItem(R.drawable.my_actived, "我").setActiveColorResource(R.color.colorPrimary).setInactiveIconResource(R.drawable.my).setInActiveColorResource(R.color.colorNavigationBarInactivedText))
                 .setFirstSelectedPosition(lastSelectedPosition)
                 .initialise();
@@ -70,7 +85,6 @@ public class MainActivity extends AppCompatActivity implements
                 hideFragment(fragmentTransaction);
                 switch (position){
                     case 0:  // index
-                        Toast.makeText(MainActivity.this, "click 0", Toast.LENGTH_SHORT).show();
                         if(mIndexFragment == null){
                             mIndexFragment = IndexFragment.newInstance("","");
                             fragmentTransaction.add(R.id.mainContainer, mIndexFragment);
@@ -79,7 +93,6 @@ public class MainActivity extends AppCompatActivity implements
                         }
                         break;
                     case 1:  // explore
-                        Toast.makeText(MainActivity.this, "click 1", Toast.LENGTH_SHORT).show();
                         if(mExploreFragment == null){
                             mExploreFragment = ExploreFragment.newInstance("","");
                             fragmentTransaction.add(R.id.mainContainer, mExploreFragment);
@@ -88,7 +101,6 @@ public class MainActivity extends AppCompatActivity implements
                         }
                         break;
                     case 2:  // post
-                        Toast.makeText(MainActivity.this, "click 2", Toast.LENGTH_SHORT).show();
                         if(mPostFragment == null){
                             mPostFragment = PostFragment.newInstance("","");
                             fragmentTransaction.add(R.id.mainContainer, mPostFragment);
@@ -97,7 +109,6 @@ public class MainActivity extends AppCompatActivity implements
                         }
                         break;
                     case 3:  // message
-                        Toast.makeText(MainActivity.this, "click 3", Toast.LENGTH_SHORT).show();
                         if(mMessageFragment == null){
                             mMessageFragment = MessageFragment.newInstance("","");
                             fragmentTransaction.add(R.id.mainContainer, mMessageFragment);
@@ -106,13 +117,19 @@ public class MainActivity extends AppCompatActivity implements
                         }
                         break;
                     case 4:  // my
-                        Toast.makeText(MainActivity.this, "click 4", Toast.LENGTH_SHORT).show();
-                        if(mMyFragment == null){
-                            mMyFragment = MyFragment.newInstance("","");
-                            fragmentTransaction.add(R.id.mainContainer, mMyFragment);
+                        boolean isLogin = false;//Todo
+                        if(isLogin) {
+                            if(mMyFragment == null){
+                                mMyFragment = MyFragment.newInstance("","");
+                                fragmentTransaction.add(R.id.mainContainer, mMyFragment);
+                            } else {
+                                fragmentTransaction.show(mMyFragment);
+                            }
                         } else {
-                            fragmentTransaction.show(mMyFragment);
+                            Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                            startActivity(intent);
                         }
+
                         break;
                     default:
                         Log.e(TAG , "navigation tab index catching error");
@@ -156,4 +173,5 @@ public class MainActivity extends AppCompatActivity implements
     public void onFragmentInteraction(Uri uri) {
         Toast.makeText(this, "MainActivity recieve message from fragment" + uri.toString(), Toast.LENGTH_SHORT).show();
     }
+
 }
